58e60dbe7f096ffb179d4e59111ab2bbf25074c8
sthira-conicle <sthira@conicle.com>
2025-06-30 17:35:38 +0700

    Setup local preview

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/conicle/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/conicle/' : '/',
   plugins: [
     vue(),
     vueJsx(),
@@ -17,4 +16,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
