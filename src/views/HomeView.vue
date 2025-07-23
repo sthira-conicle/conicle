251d882a9f042d9cf9506ee36b2665f9bf4d8b03
sthira-conicle <sthira@conicle.com>
2025-07-23 13:06:50 +0700

    23jul0106

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,20 @@
     <div class="button-container">
       <button id="click_me">Click me</button>
     </div>
-    <form>
+    <form id="filter">
       <h2>Learner</h2>
-      <label for="dept">Department</label><br>
-      <input type="text" id="dept" name="dept" value="product"><br>
-      <label for="pos">Position</label><br>
-      <input type="text" id="pos" name="pos" value="uxui"><br>
+      <label for="department">Department</label><br>
+      <input type="text" id="department" name="department" value="product"><br>
+      <label for="position">Position</label><br>
+      <input type="text" id="position" name="position" value="uxui"><br>
       <br><hr><br>
       <h2>Learning period</h2>
       <p>Select by</p>
-      <input type="radio" id="progress" name="select_by" value="progress">
+      <input type="radio" id="progress" name="learning_period" value="progress">
       <label for="progress">Has learning progress during</label><br>
-      <input type="radio" id="added" name="select_by" value="added">
+      <input type="radio" id="added" name="learning_period" value="added">
       <label for="added">Enrolled or assigned during</label><br>
-      <input type="radio" id="finished" name="select_by" value="finished">
+      <input type="radio" id="finished" name="learning_period" value="finished">
       <label for="finished">Finished during</label>
       <br><br><hr><br>
       <input type="submit" value="Apply">
