b6054ed01391f84524809e97a44d2316bebaf034
sthira-conicle <sthira@conicle.com>
2025-07-30 17:34:23 +0700

    30jul534

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,71 @@
 <script setup lang="ts">
+
+function trigger_apply_tracking_filter() {
+
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "apply_tracking_filter",
+        "filter_learner.count_filter": 5,
+        "filter_learner.select_by": "by_condition",
+        "filter_learner.select_learner": [],
+        "filter_learner.department": [
+          129,
+          528
+        ],
+        "filter_learner.position": [
+          1,
+          21
+        ],
+        "filter_learner.level": [
+          84,
+          85,
+          96
+        ],
+        "filter_learner.group": [
+          134,
+          133
+        ],
+        "filter_learner.onboard_date": [
+          "04 Aug 2025",
+          "20 Aug 2025"
+        ],
+        "filter_content.count_filter": 4,
+        "filter_content.select_by": "by_condition",
+        "filter_content.select_content": [],
+        "filter_content.category": [
+          140,
+          144
+        ],
+        "filter_content.content_type": [
+          3,
+          4
+        ],
+        "filter_content.content_provider": [
+          2,
+          3
+        ],
+        "filter_content.content_requirement": [
+          2
+        ],
+        "filter_learning_period.count_filter": 1,
+        "filter_learning_period.select_by": "is_learning_progress",
+        "filter_learning_period.period": 4,
+        "filter_learning_period.period_date": [
+          "11 Jul 2025",
+          "18 Jul 2025"
+        ],
+        "filter_learning_condition.count_filter": 2,
+        "filter_learning_condition.adding_method": [
+          1
+        ],
+        "filter_learning_condition.learning_status": [
+          4,
+          2
+        ]
+      });
+
+}
+
 </script>
 
 <template>
@@ -22,7 +89,7 @@
       <input type="radio" id="finished" name="learning_period" value="finished">
       <label for="finished">Finished during</label>
       <br><br><hr><br>
-      <input type="submit" value="Apply">
+      <input type="submit" value="Apply" onclick="trigger_apply_tracking_filter">
     </form>
   </div>
 </template>
