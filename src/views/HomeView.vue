3edc690503d0ff971712e22f0c690797b543eea5
sthira-conicle <sthira@conicle.com>
2025-07-31 23:54:46 +0700

    31jul1154

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,64 @@
 <script setup lang="ts">
 
+function generateRandomString(length) {
+  return Math.random().toString(36).substring(2, 2 + length);
+}
+
 window.dataLayer = window.dataLayer || [];
 
-function trigger_apply_tracking_filter() {
-      window.dataLayer.push({
-        event: "apply_tracking_filter",
-        "filter_learner.count_filter": 5,
-        "filter_learner.select_by": "by_condition",
-        "filter_learner.select_learner": [],
-        "filter_learner.department": [
-          129,
-          528
-        ],
-        "filter_learner.position": [
-          1,
-          21
-        ],
-        "filter_learner.level": [
-          84,
-          85,
-          96
-        ],
-        "filter_learner.group": [
-          134,
-          133
-        ],
-        "filter_learner.onboard_date": [
-          "04 Aug 2025",
-          "20 Aug 2025"
-        ],
-        "filter_content.count_filter": 4,
-        "filter_content.select_by": "by_condition",
-        "filter_content.select_content": [],
-        "filter_content.category": [
-          140,
-          144
-        ],
-        "filter_content.content_type": [
-          3,
-          4
-        ],
-        "filter_content.content_provider": [
-          2,
-          3
-        ],
-        "filter_content.content_requirement": [
-          2
-        ],
-        "filter_learning_period.count_filter": 1,
-        "filter_learning_period.select_by": "is_learning_progress",
-        "filter_learning_period.period": 4,
-        "filter_learning_period.period_date": [
-          "11 Jul 2025",
-          "18 Jul 2025"
-        ],
-        "filter_learning_condition.count_filter": 2,
-        "filter_learning_condition.adding_method": [
-          1
-        ],
-        "filter_learning_condition.status": [
-          4,
-          2
-        ]
-      });
+function push_batch_event(data, category, prefix) {
+  for (const key in data) {
+    window.dataLayer.push({
+      event: prefix ? `${prefix}${key}` : `${key}`,
+      [key]: data[key],
+      ...(category ? { event_category: category } : {})
+    });
+  }
+}
+
+function trigger_events() {
+
+push_batch_event({
+  "learner_count_filter": Math.round(Math.random()*4)+1,
+  "learner_select_by": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_select_learner": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_department": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_position": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_level": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_group": generateRandomString(Math.round(Math.random()*7)+3),
+  "learner_onboard_date": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_count_filter": Math.round(Math.random()*3)+1,
+  "content_select_by": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_select_content": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_category": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_content_type": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_content_provider": generateRandomString(Math.round(Math.random()*7)+3),
+  "content_content_requirement": generateRandomString(Math.round(Math.random()*7)+3),
+  "learning_period_count_filter": generateRandomString(Math.round(Math.random()*7)+3),
+  "learning_period_select_by": generateRandomString(Math.round(Math.random()*7)+3),
+  "learning_period_period": Math.round(Math.random()*3)+1,
+  "learning_period_period_date": generateRandomString(Math.round(Math.random()*7)+3),
+  "learning_condition_count_filter": Math.round(Math.random()*1)+1,
+  "learning_condition_adding_method": generateRandomString(Math.round(Math.random()*7)+3),
+  "learning_condition_status": generateRandomString(Math.round(Math.random()*7)+3)
+},'tracking_filter','apply_filter_');
+
+push_batch_event({
+  "hide_leraner_not_active": Math.round(Math.random()) ? true : false,
+  "include_sub_content": Math.round(Math.random()) ? true : false
+},'tracking_option','apply_option_');
+
+push_batch_event({
+  "admin": Math.round(Math.random()) ? true : false,
+  "manager": Math.round(Math.random()) ? true : false,
+  "one_level": Math.round(Math.random()) ? true : false,
+  "full_line": Math.round(Math.random()) ? true : false
+},'tracking_view_as','apply_view_as_');
+
+push_batch_event({
+  "learning_status": Math.round(Math.random()) ? true : false,
+  "completion_date": Math.round(Math.random()) ? true : false
+},'tracking_table_customization','apply_table_customization_');
 
 }
 
@@ -73,7 +69,7 @@ function trigger_apply_tracking_filter() {
     <div class="button-container">
       <button tag="click_me_test">Click me</button>
     </div>
-    <form id="tracking_filter" @submit.prevent="trigger_apply_tracking_filter">
+    <form id="tracking_filter" @submit.prevent="trigger_events">
       <h2>Learner</h2>
       <label for="department">Department</label><br>
       <input type="text" id="department" name="department" value="product"><br>
